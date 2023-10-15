e36ad5c009f0b2f4948ae31d40e35ee75cf61bf7
croil <basketrevenue@gmail.com>
2023-10-15 17:25:43 +0300

    Time fix

--- src/main/java/org/lsm/db/Cell.java
+++ src/main/java/org/lsm/db/Cell.java
@@ -9,9 +9,9 @@ import org.lsm.Entry;
 public abstract class Cell<T> implements Entry<T> {
     public interface Factory<T> {
         Cell<T> create(Entry<T> entry);
+
         Cell<T> create(T key, T value);
     }
-
 
     public abstract long valueSize();
 
